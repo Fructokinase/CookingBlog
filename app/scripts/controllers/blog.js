317042766608df536e1efd7045d4a76b0d074c8d
Alex Chen <Alex.yf.Chen@outlook.com>
2015-10-15 03:45:25 +0900

    blog model includes date, title, and body

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -7,11 +7,23 @@
  * # BlogCtrl
  * Controller of the angularGeneratorYoApp
  */
-angular.module('angularGeneratorYoApp')
-  .controller('BlogCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('cookingBlog')
+  .controller('BlogCtrl', function ($scope) {
+
+    $scope.blogContents  = [
+    {title: "some title 1", body: " testing some body 1"+ 
+    "testing some body 1 ar far away, behind the word mountains, far from. testing some body 1"+
+    "Pointing has no control about the blind texts it is an almost unorthographic tent 1"+
+    " Pityful a rethoric question ran over her cheek, then 1",
+    date: "2015-10-15"},
+    {title: "some title 2", body: " testing some body 2" +
+    "testing some body 2 testing some body 2 testing some body 2 testing some body 2"+
+    "testing some body 2 testing some body 2 testing some body 2 testing some body 2"+
+    "testing some body 2 testing some body 2 testing some body 2 testing some body 2",
+    date: "2015-10-14"},
+    {title: "some title 3", body: " testing some body 3" + 
+    "testing some body 3 testing some body 3 testing some body 3 testing some body 3"+
+    "testing some body 3 testing some body 3 testing some body 3 testing some body 3"+
+    "testing some body 3 testing some body 3 testing some body 3 testing some body 3",
+    date: "2015-10-13"}];
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,5 @@ angular.module('angularGeneratorYoApp')
       'AngularJS',
       'Karma'
     ];
+
   });
